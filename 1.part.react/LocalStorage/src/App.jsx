15566bcc09ea818d6177099da41c66c5267710b5
np-tech-324 <nishantpoojari324@gmail.com>
2025-11-07 01:30:43 +0530

    Local Storage is complete with neccessary cammand i give

--- 1.part.react/LocalStorage/src/App.jsx
+++ 1.part.react/LocalStorage/src/App.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 
 const App = () => {
+  const user = {
+    user:"Nishant",
+    age:23,
+    occupation:"Student"
+    }
+
+    localStorage.setItem("user",JSON.stringify(user))
+    console.log(user);
+    const user2 = JSON.parse(localStorage.getItem("user"))
+    console.log(user2);
+    
+    localStorage.removeItem(age)
+
+    // localStorage.clear()
   return (
     <div>App</div>
   )
